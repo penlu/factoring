231ccf120beb3c04d143a153b538329b884ef152
Eric Lu <penlume@gmail.com>
2020-06-21 20:13:28 -0400

    debug ECM for 80-digit numbers

--- factoring.py
+++ factoring.py
@@ -6,20 +6,44 @@ ECM='/home/penlu/ecm-7.0.4/ecm'
 def ecm_proc(c, b1, b2):
   return Popen([ECM, '-q', '-c', c, b1, b2], stdin=PIPE, stdout=PIPE)
 
-def ecm(n):
+def do_ecm(n, c, b1, b2):
   # run 32 parallel ECM
-  procs = [ecm_proc('2', '11e3', '1.9e6') for i in range(32)]
+  procs = [ecm_proc(str((c + 31) // 32), b1, b2) for i in range(32)]
   for p in procs:
-    p.stdin.write(n + '\n')
+    p.stdin.write(bytes(n + '\n', encoding='utf8'))
     p.stdin.flush()
 
   # accumulate results
   results = set()
   for i, p in enumerate(procs):
-    results = results + set(p.communicate().split())
+    results = results | set(map(lambda x: x.decode('utf8'), p.communicate()[0].split()))
 
   # wait for subprocesses
   for p in procs:
-    print(p.wait())
+    p.wait()
 
-  return results - n
+  return list(results - set([n]))
+
+#       digits D  optimal B1   default B2           expected curves
+#                                                       N(B1,B2,D)
+#                                              -power 1         default poly
+#          20       11e3         1.9e6             74               74 [x^1]
+#          25        5e4         1.3e7            221              214 [x^2]
+#          30       25e4         1.3e8            453              430 [D(3)]
+#          35        1e6         1.0e9            984              904 [D(6)]
+#          40        3e6         5.7e9           2541             2350 [D(6)]
+#          45       11e6        3.5e10           4949             4480 [D(12)]
+#          50       43e6        2.4e11           8266             7553 [D(12)]
+#          55       11e7        7.8e11          20158            17769 [D(30)]
+#          60       26e7        3.2e12          47173            42017 [D(30)]
+#          65       85e7        1.6e13          77666            69408 [D(30)]
+def ecm(n):
+  result = do_ecm(n, 74, '11e3', '1.9e6')
+  if result:
+    return result
+  result = do_ecm(n, 214, '5e4', '1.3e7')
+  if result:
+    return result
+  result = do_ecm(n, 430, '25e4', '1.3e8')
+  if result:
+    return result
--- worker.py
+++ worker.py
@@ -1,26 +1,42 @@
 import factordb
 import factoring
+import time
+import datetime
+
+logfile = open('log.txt', 'a')
+
+def log(s):
+  ts = time.time()
+  t = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S.%f')
+  print('[' + t + '] ' + s)
+  logfile.write('[' + t + '] ' + s + '\n')
+  logfile.flush()
 
 def main():
   while True:
-    print('getting work')
-    l = factordb_list(min_digits=80)
+    log('getting work')
+    l = factordb.factordb_list(min_digits=80)
     for n in l:
-      print('working on %s' % n)
+      log('working on %s' % n)
 
-      q = factordb_query(n)
+      q = factordb.factordb_query(n)
       if q['status'] != 'C':
-        print('already factored')
+        log('already factored')
         continue
 
-      print('running ECM')
+      log('running ECM')
       ecm_result = factoring.ecm(n)
       if ecm_result:
-        factordb_submit(q['id'], ecm_result)
+        log('success on %s' % n)
+        factordb.factordb_submit(q['id'], ecm_result)
         continue
 
-      print('running GFS')
-      gfs_result = factoring.gfs(n)
-      if gfs_result:
-        factordb_submit(q['id'], gfs_result)
-        continue
+      #print('running GFS')
+      #gfs_result = factoring.gfs(n)
+      #if gfs_result:
+      #  factordb_submit(q['id'], gfs_result)
+      #  continue
+
+    exit(0)
+
+main()
